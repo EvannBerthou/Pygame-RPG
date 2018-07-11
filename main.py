d7431bb8080f18a3d25757d654433a2e12860a0e
Sutalite <32411868+Sutalite@users.noreply.github.com>
2018-07-11 17:52:58 +0200

    Sky

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import pygame
+from pygame.locals import *
+from CONST import *
+
+window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+
+sky = pygame.image.load("Graphics/sky.png")
+Sky = window.blit(sky,(0,0))
+pygame.display.update()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
